bdd1f09123635e707712855edbf42462475e5b74
Kajal <kajalpatil158@gmail.com>
2021-02-17 17:37:47 +0530

    Added_No_Of_time_Flip

---
+++ coin2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo  "Enter number of time coin flip"
+read n
+
+for((i=0;i<n;i++))
+do
+random=$((RANDOM%2))
+
+	if [ $random -eq 0 ]
+	then
+        	echo "Head is winner"
+	else
+        	echo "Tail is winner"
+	fi
+done
